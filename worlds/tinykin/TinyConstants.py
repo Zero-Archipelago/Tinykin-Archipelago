023f1ee24c7700a7ff0a3d6360522cad61595e18
Zero <120384602+CallmeZewo@users.noreply.github.com>
2026-05-01 12:33:55 +0200

    Create TinyConstants.py
    Adding a seperate file for constants

---
+++ worlds/tinykin/TinyConstants.py
@@ -0,0 +1,29 @@
+class Area:
+
+    # Main Areas
+    TRANSIDOR_CROSSING = "Transidor Crossing"
+    CHRYSAL_WORKSHOP = "Chrysal Workshop"
+    CITY_OF_SANCTAR = "City of Sanctar"
+    FOLIANA_HEIGHTS = "Foliana Heights"
+    WATERS_OF_BALNEA = "Waters of Balnea"
+    LANDS_OF_AMBROSE = "Lands of Ambrose"
+    CELERION_PARK = "Celerion Park"
+    THE_LAST_REFUGE = "The Last Refuge"
+
+    # Sub Areas
+    CHALLENGES = "Challenges"
+
+    # Arealess
+    SIKARU = "Sikaru"
+
+class Tinykin:
+    CARRIER = "Carrier"
+    EXPLOSIVE = "Explosive"
+    LADDER = "Ladder"
+    ELECTRIC = "Electric"
+    BRIDGE = "Bridge"
+
+class Collectibles:
+    SOAPBOARD = "Soapboard"
+    BUBBLE = "Bubble"
+    POLLEN = "Pollen"
